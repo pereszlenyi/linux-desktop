8becf7cf4dccc1d9ae299a66ad2ccf86a440010b
Attila Pereszlényi <attila@pereszlenyi.com>
2023-04-07 20:41:23 +0200

    Script for asking a yes-no question.

---
+++ scripts/yes_or_no.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script asks a yes-no question.
+# It returns 0 if the answer was yes or 1 if the answer was no.
+# Usage: yes_or_no.sh "Question"
+
+ECHO=/usr/bin/echo
+TR=/usr/bin/tr
+
+function die {
+	$ECHO "Error: $1"
+	exit 2
+}
+
+for FILE in "$ECHO" "$TR" ; do
+	[ -x "$FILE" ] || die "'$FILE' doesn't exist or not executable."
+done
+
+[ ! $# == 1 ] && die "Usage: $0 \"Question\""
+
+while true; do
+	read -p "$1 [yes/no]: " ANSWER
+	ANSWER=$($TR "[:upper:]" "[:lower:]" <<<"$ANSWER")
+	([ "$ANSWER" == "yes" ] || [ "$ANSWER" == "y" ]) && exit 0
+	([ "$ANSWER" == "no" ] || [ "$ANSWER" == "n" ]) && exit 1
+	$ECHO "Please type in \"y\" or \"n\"."
+done
