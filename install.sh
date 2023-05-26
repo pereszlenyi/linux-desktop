4b7a3e7a345a2d767530356355ec37a633c4bb93
Attila Pereszlényi <attila@pereszlenyi.com>
2023-05-26 21:27:57 +0200

    Reworked the installation of Ansible.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ECHO=/usr/bin/echo
 SUDO=/usr/bin/sudo
 DIRNAME=/usr/bin/dirname
 APTGET=/usr/bin/apt-get
+ADD_APT_REPO=/usr/bin/add-apt-repository
 ANSIBLE=/usr/bin/ansible-playbook
 
 function die {
@@ -14,8 +15,8 @@ function die {
 	exit 1
 }
 
-for FILE in "$ECHO" "$SUDO" "$DIRNAME" "$APTGET" ; do
-	[ -x "$FILE" ] || die "'$FILE' doesn't exist or not executable."
+for FILE in "$ECHO" "$SUDO" "$DIRNAME" "$APTGET" "$ADD_APT_REPO" ; do
+	[ -x "$FILE" ] || die "'$FILE' doesn't exist or it's not executable."
 done
 
 [ $# == 0 ] || die "\"$0\" doesn't accept any parameters."
@@ -27,18 +28,27 @@ function check_ansible {
 DIR=$($DIRNAME "$0")
 cd $DIR || die "Can't enter $DIR."
 
-for FILE in update_ansible.ansible.yml setup.ansible.yml ; do
+for FILE in setup.ansible.yml ansible.cfg ; do
 	[ -r "./$FILE" ] || die "$FILE is missing or not readable."
 done
 
 for FILE in yes_or_no.sh setup_gpg.sh ; do
-	[ -x "./scripts/$FILE" ] || die "'$FILE' doesn't exist or not executable."
+	[ -x "./scripts/$FILE" ] || die "'$FILE' doesn't exist or it's not executable."
 done
 
 $ECHO "=== Setting up your Linux system ==="
 $ECHO ""
 
-check_ansible || ($ECHO "=== Installing Ansible ===" && $SUDO $APTGET --assume-yes install ansible && $ECHO "")
+check_ansible || ($ECHO "=== Installing Ansible ===" && \
+	$SUDO $APTGET update && \
+	$SUDO $APTGET --assume-yes --fix-broken install && \
+	$SUDO $APTGET --assume-yes dist-upgrade && \
+	$SUDO $ADD_APT_REPO --yes --update ppa:ansible/ansible && \
+	$SUDO $APTGET --assume-yes install ansible aptitude && \
+	$SUDO $APTGET --assume-yes autoremove && \
+	$SUDO $APTGET --assume-yes clean && \
+	$ECHO "=== Ansible is installed ===" && \
+	$ECHO "")
 check_ansible || die "Ansible is not installed."
 
 $ECHO "The following information will be used to configure Git."
@@ -59,13 +69,12 @@ if ./scripts/yes_or_no.sh \
 fi
 $ECHO ""
 
-$ECHO "=== Running Ansible playbooks ==="
-$ANSIBLE ./update_ansible.ansible.yml --ask-become-pass && \
+$ECHO "=== Running Ansible playbook ==="
 $ANSIBLE ./setup.ansible.yml --ask-become-pass \
 	--extra-vars "FULLNAME=\"$FULLNAME\" EMAIL=\"$EMAIL\" \
 	INSTALL_CONTAINERIZATION_TECHS=\"$INSTALL_CONTAINERIZATION_TECHS\"" && \
 $ECHO "=== Install was SUCCESSFUL ===" || \
-die "Failed to run Ansible playbooks. You can try shutting down and restarting WSL (by \"wsl.exe --shutdown\") then running $0 again."
+die "Failed to run Ansible playbook. You can try shutting down and restarting WSL (by \"wsl.exe --shutdown\") then running $0 again."
 
 if [ -e /var/run/reboot-required ] ; then
 	$ECHO ""
