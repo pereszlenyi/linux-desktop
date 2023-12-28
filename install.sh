77979505ab003772d35818a5cac4c1f77589e8cd
Attila Pereszlényi <attila@pereszlenyi.com>
2023-12-28 16:43:18 +0100

    The install script can now be run by a user without sudo rights.

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ ECHO=/usr/bin/echo
 SUDO=/usr/bin/sudo
 DIRNAME=/usr/bin/dirname
 BASENAME=/usr/bin/basename
+GROUPS_CMD=/usr/bin/groups
+GREP=/usr/bin/grep
 APTGET=/usr/bin/apt-get
 ADD_APT_REPO=/usr/bin/add-apt-repository
 ANSIBLE=/usr/bin/ansible-playbook
@@ -16,7 +18,7 @@ function die {
 	exit 1
 }
 
-for FILE in "$ECHO" "$SUDO" "$DIRNAME" "$BASENAME" "$APTGET" "$ADD_APT_REPO" ; do
+for FILE in "$ECHO" "$SUDO" "$DIRNAME" "$BASENAME" "$GROUPS_CMD" "$GREP" "$APTGET" "$ADD_APT_REPO" ; do
 	[ -x "$FILE" ] || die "'$FILE' doesn't exist or it's not executable."
 done
 
@@ -30,7 +32,9 @@ function check_ansible {
 DIR=$($DIRNAME "$0")
 cd $DIR || die "Can't enter $DIR."
 
-for FILE in setup.ansible.yml ansible.cfg ; do
+for FILE in setup.ansible.yml ansible.cfg include_tasks/apt_repository.ansible.yml \
+	include_tasks/create_upgrade_source_for_ppa.ansible.yml \
+	include_tasks/create_upgrade_source_for_repository.ansible.yml ; do
 	[ -r "./$FILE" ] || die "$FILE is missing or not readable."
 done
 
@@ -41,18 +45,29 @@ done
 $ECHO "=== Setting up your Linux system ==="
 $ECHO ""
 
+if $GROUPS_CMD | $GREP "sudo" &>/dev/null ; then
+	ANSIBLE_COMMAND="$ANSIBLE --ask-become-pass"
+else
+	$ECHO "Note: The current user doesn't have sudo rights."
+	check_ansible || die "You need to run this script first with a user that has sudo rights."
+	$ECHO "Systemwide changes will be skipped."
+	$ECHO ""
+	ANSIBLE_COMMAND="$ANSIBLE --skip-tags system"
+fi
+
 INSTALL_COMMAND="$ECHO \"=== Installing Ansible ===\" && \
 	$APTGET update && \
 	$APTGET --assume-yes --fix-broken install && \
 	$APTGET --assume-yes dist-upgrade && \
 	$ADD_APT_REPO --yes --update ppa:ansible/ansible && \
-	$APTGET --assume-yes install ansible aptitude grep && \
+	$APTGET --assume-yes install ansible aptitude && \
 	$APTGET --assume-yes autoremove && \
 	$APTGET --assume-yes clean && \
 	$ECHO \"=== Ansible is installed ===\" && \
 	$ECHO \"\""
 
-check_ansible || $SUDO /bin/bash -c "$INSTALL_COMMAND"
+check_ansible || $SUDO /bin/bash -c "$INSTALL_COMMAND" || \
+die "Unable to install Ansible."
 check_ansible || die "Ansible is not installed."
 
 $ECHO "The following information will be used to configure Git."
@@ -84,11 +99,12 @@ fi
 $ECHO ""
 
 $ECHO "=== Running Ansible playbook ==="
-$ANSIBLE ./setup.ansible.yml --ask-become-pass \
+$ANSIBLE_COMMAND \
 	--extra-vars "FULLNAME=\"$FULLNAME\" EMAIL=\"$EMAIL\" \
 	INSTALL_JAVA_TOOLS=\"$INSTALL_JAVA_TOOLS\" \
 	JDK_VERSION=\"$JDK_VERSION\" \
-	INSTALL_CONTAINERIZATION_TECHS=\"$INSTALL_CONTAINERIZATION_TECHS\"" && \
+	INSTALL_CONTAINERIZATION_TECHS=\"$INSTALL_CONTAINERIZATION_TECHS\"" \
+	./setup.ansible.yml && \
 $ECHO "=== Install was SUCCESSFUL ===" || \
 die "Failed to run Ansible playbook. You can try shutting down and restarting WSL (by \"wsl.exe --shutdown\") then running $FILENAME again."
 
