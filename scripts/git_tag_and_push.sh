db60e4e254a57115c36bcad5d68b27c2f6b9c605
Attila Pereszlényi <attila@pereszlenyi.com>
2024-01-06 18:58:37 +0100

    Script for creating a tag and pushing it to the remote Git repository.

---
+++ scripts/git_tag_and_push.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script creates a tag and pushes it to the remote Git repository.
+# The single argument is the name of the tag to create.
+
+ECHO="builtin echo"
+GIT=/usr/bin/git
+
+function die {
+	$ECHO -e "\033[00;31mError: $1\033[00m" >&2
+	exit 1
+}
+
+[ -x "$GIT" ] || die "Can't find Git."
+
+# Checking for command-line parameters.
+[ $# == 1 ] || die "Usage $0 name_of_the_tag"
+
+# Checking for modified files.
+[ "$($GIT status --porcelain --untracked-files=no)" == "" ] || \
+die "There are modified files that are not committed."
+
+$ECHO "=== Creating tag ===" && \
+$GIT tag --sign "$1" || die "Git tag failed."
+
+$ECHO -e "\n=== Showing tag details ===" && \
+$GIT show --no-patch --abbrev-commit "$1" || die "Git show failed."
+
+$ECHO -e "\n=== Pushing tag ===" && \
+$GIT push --quiet origin "$1" || die "Git push failed."
+$ECHO "OK"
